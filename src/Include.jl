826c9c178ac90d7871c0021b5de31688c82fb03e
Jeffrey Varner <96922336+Paliquant@users.noreply.github.com>
2022-01-07 15:23:38 -0500

    Fix for compat issue
    PR to fix compat section issue in Project.toml. Removed Reexport

--- src/Include.jl
+++ src/Include.jl
@@ -2,17 +2,16 @@
 _PATH_TO_SRC = dirname(pathof(@__MODULE__))
 
 # load external packages that we depend upon -
-using Reexport
-@reexport using DataFrames
-@reexport using CSV
+using DataFrames
+using CSV
 using HTTP
 using TOML
 using Dates
 using JSON
 
 # load my codes -
-include(joinpath(_PATH_TO_SRC,"Types.jl"))
-include(joinpath(_PATH_TO_SRC,"Base.jl"))
-include(joinpath(_PATH_TO_SRC,"Network.jl"))
-include(joinpath(_PATH_TO_SRC,"Factory.jl"))
-include(joinpath(_PATH_TO_SRC,"Handler.jl"))
+include(joinpath(_PATH_TO_SRC, "Types.jl"))
+include(joinpath(_PATH_TO_SRC, "Base.jl"))
+include(joinpath(_PATH_TO_SRC, "Network.jl"))
+include(joinpath(_PATH_TO_SRC, "Factory.jl"))
+include(joinpath(_PATH_TO_SRC, "Handler.jl"))
