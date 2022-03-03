08fbe1244a56c53bb82d9965fae5a772262b46d3
George Pan <98866751+ycpan1012@users.noreply.github.com>
2022-03-03 00:08:15 -0500

    update test

--- test/test_ticker_details_api_call.jl
+++ test/test_ticker_details_api_call.jl
@@ -1,10 +1,10 @@
-using PQPolygonSDK
+using testPQPolygonSDK
 using Dates
 using DataFrames
 
 # build a user model -
 options = Dict{String,Any}()
-options["email"] = "jvarner@paliquant.com"
+options["email"] = "yp392@cornell.edu"
 options["apikey"] = "abc12345" # do _not_ check in a real API key 
 
 # build the user model -
@@ -13,6 +13,7 @@ user_model = model(PQPolygonSDKUserModel, options)
 # now that we have the user_model, let's build an endpoint model -
 endpoint_options = Dict{String,Any}()
 endpoint_options["ticker"] = "AAPL"
+endpoint_options["date"] = "N/A"
 endpoint_model = model(PolygonTickerDetailsEndpointModel, user_model, endpoint_options)
 
 # build the url -
