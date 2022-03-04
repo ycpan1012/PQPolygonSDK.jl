a6791effde89ae7dd5b0fccc1db513af529dc74b
George Pan <98866751+ycpan1012@users.noreply.github.com>
2022-03-04 16:56:48 -0500

    Create test_stock_financials_api_call.jl

---
+++ test/test_stock_financials_api_call.jl
@@ -0,0 +1,34 @@
+using testPQPolygonSDK #test package -ycpan
+using Dates
+using DataFrames
+
+# build a user model -
+options = Dict{String,Any}()
+options["email"] = "yp392@cornell.edu"
+options["apikey"] = "abc1234" # do _not_ check in a real API key 
+
+# build the user model -
+user_model = model(PQPolygonSDKUserModel, options)
+
+# now that we have the user_model, let's build an endpoint model -
+endpoint_options = Dict{String,Any}()
+endpoint_options["ticker"] = "AAPL"
+endpoint_options["cik"] = "N/A"
+endpoint_options["company_name"] = "N/A"
+endpoint_options["sic"] = "N/A"
+endpoint_options["filing_date"] = "N/A"
+endpoint_options["period_of_report_date"] = "N/A"
+endpoint_options["timeframe"] = "N/A"
+endpoint_options["include_sources"] = "N/A"
+endpoint_options["order"] = "N/A"
+endpoint_options["limit"] = "N/A"
+endpoint_options["sort"] = "N/A"
+
+endpoint_model = model(PolygonStockFinancialsEndpointModel, user_model, endpoint_options)
+
+# build the url -
+base = "https://api.polygon.io"
+my_url_string = url(base, endpoint_model)
+
+# make the call -
+(header, data) = api(PolygonStockFinancialsEndpointModel, my_url_string)
