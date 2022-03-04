13d487817385bb02e95e7a25cd8a451e494d0c86
George Pan <98866751+ycpan1012@users.noreply.github.com>
2022-03-04 16:51:35 -0500

    Create test_market_holidays_api_call.jl

---
+++ test/test_market_holidays_api_call.jl
@@ -0,0 +1,22 @@
+using testPQPolygonSDK #test package -ycpan
+using Dates
+using DataFrames
+
+# build a user model -
+options = Dict{String,Any}()
+options["email"] = "yp392@cornell.edu"
+options["apikey"] = "abc1234" # do _not_ check in a real API key 
+
+# build the user model -
+user_model = model(PQPolygonSDKUserModel, options)
+
+# now that we have the user_model, let's build an endpoint model -
+endpoint_options = Dict{String,Any}()
+endpoint_model = model(PolygonMarketHolidaysEndpointModel, user_model, endpoint_options)
+
+# build the url -
+base = "https://api.polygon.io"
+my_url_string = url(base, endpoint_model)
+
+# make the call -
+(header, data) = api(PolygonMarketHolidaysEndpointModel, my_url_string)
